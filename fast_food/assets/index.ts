de5677ac8c7b52474e48e2877ac820c1eb6642ec
Arya <jackrack040@gmail.com>
2025-07-06 22:20:01 -0400

    asset index set up

---
+++ fast_food/assets/index.ts
@@ -0,0 +1,202 @@
+import arrowBack from "./icons/arrow-back.png";
+import arrowDown from "./icons/arrow-down.png";
+import arrowRight from "./icons/arrow-right.png";
+import bag from "./icons/bag.png";
+import check from "./icons/check.png";
+import clock from "./icons/clock.png";
+import dollar from "./icons/dollar.png";
+import envelope from "./icons/envelope.png";
+import home from "./icons/home.png";
+import location from "./icons/location.png";
+import logout from "./icons/logout.png";
+import minus from "./icons/minus.png";
+import pencil from "./icons/pencil.png";
+import person from "./icons/person.png";
+import phone from "./icons/phone.png";
+import plus from "./icons/plus.png";
+import search from "./icons/search.png";
+import star from "./icons/star.png";
+import trash from "./icons/trash.png";
+import user from "./icons/user.png";
+
+import avatar from "./images/avatar.png";
+import avocado from "./images/avocado.png";
+import bacon from "./images/bacon.png";
+import burgerOne from "./images/burger-one.png";
+import burgerTwo from "./images/burger-two.png";
+import buritto from "./images/buritto.png";
+import cheese from "./images/cheese.png";
+import coleslaw from "./images/coleslaw.png";
+import cucumber from "./images/cucumber.png";
+import emptyState from "./images/empty-state.png";
+import fries from "./images/fries.png";
+import loginGraphic from "./images/login-graphic.png";
+import logo from "./images/logo.png";
+import mozarellaSticks from "./images/mozarella-sticks.png";
+import mushrooms from "./images/mushrooms.png";
+import onionRings from "./images/onion-rings.png";
+import onions from "./images/onions.png";
+import pizzaOne from "./images/pizza-one.png";
+import salad from "./images/salad.png";
+import success from "./images/success.png";
+import tomatoes from "./images/tomatoes.png";
+
+export const CATEGORIES = [
+  {
+    id: "1",
+    name: "All",
+  },
+  {
+    id: "2",
+    name: "Burger",
+  },
+  {
+    id: "3",
+    name: "Pizza",
+  },
+  {
+    id: "4",
+    name: "Wrap",
+  },
+  {
+    id: "5",
+    name: "Burrito",
+  },
+];
+
+export const offers = [
+  {
+    id: 1,
+    title: "SUMMER COMBO",
+    image: burgerOne,
+    color: "#D33B0D",
+  },
+  {
+    id: 2,
+    title: "BURGER BASH",
+    image: burgerTwo,
+    color: "#DF5A0C",
+  },
+  {
+    id: 3,
+    title: "PIZZA PARTY",
+    image: pizzaOne,
+    color: "#084137",
+  },
+  {
+    id: 4,
+    title: "BURRITO DELIGHT",
+    image: buritto,
+    color: "#EB920C",
+  },
+];
+
+export const sides = [
+  {
+    name: "Fries",
+    image: fries,
+    price: 3.5,
+  },
+  {
+    name: "Onion Rings",
+    image: onionRings,
+    price: 4.0,
+  },
+  {
+    name: "Mozarella Sticks",
+    image: mozarellaSticks,
+    price: 5.0,
+  },
+  {
+    name: "Coleslaw",
+    image: coleslaw,
+    price: 2.5,
+  },
+  {
+    name: "Salad",
+    image: salad,
+    price: 4.5,
+  },
+];
+
+export const toppings = [
+  {
+    name: "Avocado",
+    image: avocado,
+    price: 1.5,
+  },
+  {
+    name: "Bacon",
+    image: bacon,
+    price: 2.0,
+  },
+  {
+    name: "Cheese",
+    image: cheese,
+    price: 1.0,
+  },
+  {
+    name: "Cucumber",
+    image: cucumber,
+    price: 0.5,
+  },
+  {
+    name: "Mushrooms",
+    image: mushrooms,
+    price: 1.2,
+  },
+  {
+    name: "Onions",
+    image: onions,
+    price: 0.5,
+  },
+  {
+    name: "Tomatoes",
+    image: tomatoes,
+    price: 0.7,
+  },
+];
+
+export const images = {
+  avatar,
+  avocado,
+  bacon,
+  burgerOne,
+  burgerTwo,
+  buritto,
+  cheese,
+  coleslaw,
+  cucumber,
+  emptyState,
+  fries,
+  loginGraphic,
+  logo,
+  mozarellaSticks,
+  mushrooms,
+  onionRings,
+  onions,
+  pizzaOne,
+  salad,
+  success,
+  tomatoes,
+  arrowBack,
+  arrowDown,
+  arrowRight,
+  bag,
+  check,
+  clock,
+  dollar,
+  envelope,
+  home,
+  location,
+  logout,
+  minus,
+  pencil,
+  person,
+  phone,
+  plus,
+  search,
+  star,
+  trash,
+  user,
+};
